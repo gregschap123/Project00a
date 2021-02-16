439b4e32ac16255f2ab3164a3a3d6ae9036e1810
eric <gorskie@moravian.edu>
2021-02-16 09:50:45 -0500

    added Team02 and my name to the out

---
+++ Team02.java
@@ -0,0 +1,23 @@
+package Project00a;
+
+import java.util.*;
+import Project00a.Team;
+import Project00a.Member;
+import Project00a.TeamMember;
+
+
+public class Team02 extends Team
+{
+    public Team02(String name)
+    {
+        super(name);
+        createTeamMembers();
+    }
+
+    public void createTeamMembers()
+    {
+        members.add(new TeamMember("Eric Gorski"));
+        // create a new member class for each team member
+        // and add it to the team members list
+    }
+}
