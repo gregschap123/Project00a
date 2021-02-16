f132b63c3a0264cf685a6f82aedb233ea2a46d84
maxwellschuman <IaPIjDKh>
2021-02-16 09:48:17 -0500

    added my name

--- Team4.java
+++ Team4.java
@@ -17,6 +17,7 @@ public class Team4 extends Team
     public void createTeamMembers()
     {
         members.add(new TeamMember("Justin Szaro"));
+        members.add(new TeamMember("Maxwell Schuman"));
 
         // create a new member class for each team member
         // and add it to the team members list
